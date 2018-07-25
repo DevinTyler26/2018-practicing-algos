43823556add3e42c70495ecfef0fc2985eaeb7ee
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-07-25 08:58:30 -0700

    python array rotate 90

---
+++ flipMatrix90.py
@@ -0,0 +1,18 @@
+def flip90(matrix):
+  matrix.reverse()
+  
+  for i in range (len(matrix)):
+    for j in range(i):
+      matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+  return matrix
+
+matrix = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
+
+flip90(matrix)
+print(matrix[0])
+print(matrix[1])
+print(matrix[2])
