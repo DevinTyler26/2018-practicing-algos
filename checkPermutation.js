d7005d1da612ef3e957b2de2bce305fbcd0fef69
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-08-06 09:37:22 -0700

    working well now

--- checkPermutation.js
+++ checkPermutation.js
@@ -1,17 +1,20 @@
 const isPerm = (str1, str2) => {
   let perm = true;
   let map = {};
+  let n = str1.length
+  let m = str2.length
 
-  for (i=0; i<str1.length; i++ ){
+  if (n !== m) return perm = false
+
+  for (i=0; i<n; i++ ){
     if(map[str1[i]]){
       map[str1[i]] += 1;
     } else {
       map[str1[i]] = 1;
     }
-    console.log(map)
   }
 
-  for (i=0; i<str2.length; i++){
+  for (i=0; i<m; i++){
     if (!map[str2[i]]) {
       perm = false
     }
@@ -21,7 +24,7 @@ const isPerm = (str1, str2) => {
   return perm;
 }
 
-string1 = ['d','o','g']
+string1 = ['d','o','g',]
 string2 = ['g','o','d']
 
 isPerm(string1, string2)
